65884de80375016a025369e4ccb4ced162466fc5
satyajitk018 <satyajitk2006@gmail.com>
2026-09-20 00:50:10 +0530

    Time: 0 ms (100%), Space: 263.4 MB (20.46%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,33 +1,33 @@
-class Solution {
-public:
-    vector<int> reverse(vector<int>& nums,int start, int end){
-        while(start <= end){
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
-        } 
-        return nums;
-    }
-    void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k % n;
-
-        reverse(nums,0,n-k-1);
-        reverse(nums,n-k,n-1);
-        reverse(nums,0,n-1);
-        
-    }
-};
-
 // class Solution {
 // public:
+//     vector<int> reverse(vector<int>& nums,int start, int end){
+//         while(start <= end){
+//             int temp = nums[start];
+//             nums[start] = nums[end];
+//             nums[end] = temp;
+//             start++;
+//             end--;
+//         } 
+//         return nums;
+//     }
 //     void rotate(vector<int>& nums, int k) {
-        
-//         reverse(nums.begin(),nums.end()-k);
-//         reverse(nums.end()-k,nums.end());
-//         reverse(nums.begin(),nums.end());
+//         int n = nums.size();
+//         k = k % n;
+
+//         reverse(nums,0,n-k-1);
+//         reverse(nums,n-k,n-1);
+//         reverse(nums,0,n-1);
         
 //     }
 // };
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        reverse(nums.begin(),nums.end()-k);
+        reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end());
+        
+    }
+};
